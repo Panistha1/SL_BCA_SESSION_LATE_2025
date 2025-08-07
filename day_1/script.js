7ddbdb75d1db21642360b59410dc9359c0a89969
Panistha1 <withpanistha1@gmail.com>
2025-08-07 09:57:25 +0545

    SL: JavaScript DAY 1 - git demo

---
+++ day_1/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("Trying git and github for the first time");
+console.log("Repo created &ssh key generated");
+console.log("Repo cloned & code added ");
+console.log("Code commited & pushed");
+console.log("Code pulled & merged");
+console.log("Code pushed and pulled");
+console.log("Code pulled and merged");
+
+
